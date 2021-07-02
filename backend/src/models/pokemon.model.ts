b79c15989a4afc3c343190f66854495febebf11b
jgfn1 <jgfn@cin.ufpe.br>
2021-07-02 17:28:35 -0300

    Revert "additionalProperties constraint removed"
    This reverts commit 362b3d8de8dbaa200735be3145d1bb0c1e772007.

--- backend/src/models/pokemon.model.ts
+++ backend/src/models/pokemon.model.ts
@@ -1,10 +1,6 @@
 import {Entity, model, property} from '@loopback/repository';
 
-@model({
-  settings: {
-    strict: false
-  }
-})
+@model()
 export class Pokemon extends Entity {
   @property({
     type: 'number',
